7895072c56071a8240586313e9402baf4cd11f2f
vg1ne <mbox696@gmail.com>
2018-03-02 22:55:22 +0200

    added table with sorting trigger

--- folio-app/src/app/contacts/containers/contacts/contacts.component.ts
+++ folio-app/src/app/contacts/containers/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Sort} from "@angular/material";
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
-
-  constructor() { }
+  desserts = [
+    {name: 'Frozen yogurt', calories: '159', fat: '6', carbs: '24', protein: '4'},
+    {name: 'Ice cream sandwich', calories: '237', fat: '9', carbs: '37', protein: '4'},
+    {name: 'Eclair', calories: '262', fat: '16', carbs: '24', protein: '6'},
+    {name: 'Cupcake', calories: '305', fat: '4', carbs: '67', protein: '4'},
+    {name: 'Gingerbread', calories: '356', fat: '16', carbs: '49', protein: '4'},
+  ];
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  sortData(sort: Sort) {
+    console.log('sorting')
+  }
 }
--- folio-app/src/app/shared/shared.module.ts
+++ folio-app/src/app/shared/shared.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import {components} from "./index";
+import {MatPaginatorModule, MatSortModule} from "@angular/material";
 
 @NgModule({
   imports: [
     CommonModule,
     MatProgressSpinnerModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   declarations: [
     ...components,
   ],
   exports: [
     ...components,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class SharedModule { }
